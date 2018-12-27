ded9885a2603d2da0f3ba4e586e20b5247fed36c
Sawered <sawered@gmail.com>
2018-12-28 00:22:25 +0300

    dynamic linking

--- app/build.rs
+++ app/build.rs
@@ -1,5 +1,16 @@
+use std::env;
+
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+//    for (x,y) in env::vars(){
+//        println!("{:?} {:?}",x,y);
+//    }
+    let ld_path = env::var("LD_LIBRARY_PATH").unwrap();
+
     println!("cargo:rustc-link-lib=dylib=clib_shared");
-//    println!("cargo:rustc-link-lib=static=app_crate");
-    println!("cargo:rustc-link-search=native=../../shared_ffi_test/clib-shared/cmake-build-debug/")
+    println!("cargo:rustc-link-search=native=../../shared_ffi_test/clib-shared/cmake-build-debug/");
+    println!("cargo:rustc-link-lib=dylib=app_crate");
+//    println!("cargo:rustc-link-search=native=./,{}",
+//             ld_path.replace(":",",")
+//    );
 }
