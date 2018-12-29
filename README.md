7c1699a4be6a80e56953a06d57e06fb185b50823
Sawered <sawered@gmail.com>
2018-12-29 10:06:04 +0300

    attempt to fix build configs

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+http://siciarz.net/24-days-of-rust-calling-rust-from-other-languages/
+
+#### Issues
+
 [Re-exporting C symbols for cdylib #36342](https://github.com/rust-lang/rust/issues/36342)
 
 [Compiling to Shared Library doesn't always expose reexported symbols #50007](https://github.com/rust-lang/rust/issues/50007)
@@ -5,3 +9,5 @@
 [Libraries not provided as arguments to the linker #25691](https://github.com/rust-lang/rust/issues/25691)
 
 [Linking an rlib with a dylib may not actually link the rlib #14344](https://github.com/rust-lang/rust/issues/14344)
+
+https://users.rust-lang.org/t/exporting-dynamic-symbols-from-executable/6103
--- app/build.rs
+++ app/build.rs
@@ -1,17 +1,17 @@
 use std::env;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    //https://doc.rust-lang.org/cargo/reference/environment-variables.html
-//    for (x,y) in env::vars(){
-//        println!("{:?} {:?}",x,y);
-//    }
-    let ld_path = env::var("LD_LIBRARY_PATH").unwrap();
-
-    println!("cargo:rustc-link-lib=dylib=clib_shared");
-    println!("cargo:rustc-link-search=native=../../shared_ffi_test/clib-shared/cmake-build-debug/");
+//    let out_dir = env::var("OUT_DIR").unwrap();
+//    //https://doc.rust-lang.org/cargo/reference/environment-variables.html
+////    for (x,y) in env::vars(){
+////        println!("{:?} {:?}",x,y);
+////    }
+//    let ld_path = env::var("LD_LIBRARY_PATH").unwrap();
+//
+//    println!("cargo:rustc-link-lib=dylib=clib_shared");
+//    println!("cargo:rustc-link-search=native=../../shared_ffi_test/clib-shared/cmake-build-debug/");
 //    println!("cargo:rustc-link-lib=dylib=app_crate");
-//    println!("cargo:rustc-link-search=native=./,{}",
-//             ld_path.replace(":",",")
-//    );
+////    println!("cargo:rustc-link-search=native=./,{}",
+////             ld_path.replace(":",",")
+////    );
 }
